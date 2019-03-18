36395e3bbee27c1af37031c603133c7e8fb1068b
IanMcT <ian.mctavish@tldsb.on.ca>
2019-03-18 05:33:18 -0400

    Add files via upload

---
+++ AddingHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphismExample
+{
+    class AddingHelper
+    {
+        public AddingHelper() { }
+
+        public int Add(int x, int y)
+        {
+            return (x + y);
+        }
+
+        public int Add(int x, int y, int z)
+        {
+            return (x + y + z);
+        }
+
+        public int Add(int[] numbers)
+        {
+            int total = 0;
+            foreach (int x in numbers)
+            {
+                total += x;
+            }
+            return total;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace polymorphismExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AddingHelper ah = new AddingHelper();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExample1_Click(object sender, RoutedEventArgs e)
+        {
+            lblOutput.Content =  ah.Add(1, 2).ToString();
+        }
+
+        private void btnExample2_Click(object sender, RoutedEventArgs e)
+        {
+            lblOutput.Content = ah.Add(1, 2, 3).ToString();
+        }
+
+        private void btnExample3_Click(object sender, RoutedEventArgs e)
+        {
+            int[] mynumbers = { 1, 2, 3, 4, 5, 6 };
+            lblOutput.Content = ah.Add(mynumbers).ToString();
+        }
+    }
+}
